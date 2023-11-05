a4c89b50beeb42552aab019e713e05ec63050fe7
Serdar Mor <serdar34mor@gmail.com>
2023-11-05 22:53:09 +0300

    admin endpoint to create travel

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\Api\V1\TourController;
+use App\Http\Controllers\Api\V1\Admin;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class,'index']);
+
+Route::prefix('admin')->middleware(['auth:sanctum','role:admin'])->group(function() {
+
+    Route::post('travels', [Admin\TravelController::class,'store']);
+});
+
+Route::post('login',LoginController::class);
